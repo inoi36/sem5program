5f6a292e0b209da3682ef24569ec0f7dcf8392ab
inoi36 <inoi9094@gmail.com>
2024-09-20 23:35:49 +0530

    Create pr1.java

---
+++ pr1.java
@@ -0,0 +1,10 @@
+public class pr1 {
+    public static void main (String[] args)
+    {
+        double pi = 3.14,r = 5,area;
+        area=pi*r*r;
+
+        System.out.println("Area of Circle ="+area);
+
+    }
+}
