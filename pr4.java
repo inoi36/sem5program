23706b79a5147cf049015492e60a9123354b7d06
inoi36 <inoi9094@gmail.com>
2024-09-20 23:38:45 +0530

    Create pr4.java

---
+++ pr4.java
@@ -0,0 +1,16 @@
+class pr4{
+	
+	public static void main (String[] args) {
+    int a =10,b =10,c =11;
+    
+    if(a> b && a>c)
+    {
+    	System.out.println("A is greater than b and c");
+    	
+    }else if(b > a && b>c){
+    	System.out.println("B is greater than a and c");
+    }else{
+    	System.out.println("C is greater tha b and c");
+    }
+    }
+}
